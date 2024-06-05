5122ba0ee773aa3c3acc151a1bbced1121820d9e
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-06-05 12:17:10 +0300

    Simplified using players array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var currentPlayer;
 var state = [];
 var moves;
+var players = ["O", "X", "O"];
 
 function start() {
 	currentPlayer = 0;
@@ -43,44 +44,24 @@ function clickNew() {
 
 function checkState() {
 	var isWon = 0;
-	if (window.currentPlayer == 0) {
-		document.getElementById("game_stats").textContent = "X to click";
-	} else {
-		document.getElementById("game_stats").textContent = "O to click";
-	}
+	document.getElementById("game_stats").textContent = players[currentPlayer + 1] + " to click";
 	for (var i = 0; i < 3; i++)
 		if ((state[i] == state[i + 3]) && (state[i] == state[i + 6]) && (state[i] > 0)) {
 			isWon = 1;
-			if (state[i] == 1) {
-				document.getElementById("game_stats").textContent = "X has won!";
-			} else {
-				document.getElementById("game_stats").textContent = "O has won!";
-			}
+			document.getElementById("game_stats").textContent = players[state[i]] + " has won!";
 		}
 	for (var i = 0; i < 7; i += 3)
 		if ((state[i] == state[i + 1]) && (state[i] == state[i + 2]) && (state[i] > 0)) {
 			isWon = 1;
-			if (window.state[i] == 1) {
-				document.getElementById("game_stats").textContent = "X has won!";
-			} else {
-				document.getElementById("game_stats").textContent = "O has won!";
-			}
+			document.getElementById("game_stats").textContent = players[state[i]] + " has won!";
 		}
 	if ((state[0] == state[4]) && (state[0] == state[8]) && (state[0] > 0)) {
 		isWon = 1;
-		if (window.state[0] == 1) {
-			document.getElementById("game_stats").textContent = "X has won!";
-		} else {
-			document.getElementById("game_stats").textContent = "O has won!";
-		}
+		document.getElementById("game_stats").textContent = players[state[0]] + " has won!";
 	}
 	if ((state[2] == state[4]) && (state[2] == state[6]) && (state[2] > 0)) {
 		isWon = 1;
-		if (window.state[2] == 1) {
-			document.getElementById("game_stats").textContent = "X has won!";
-		} else {
-			document.getElementById("game_stats").textContent = "O has won!";
-		}
+		document.getElementById("game_stats").textContent = players[state[2]] + " has won!";
 	}
 	if ((isWon === 0) && (moves === 9)) {
 		document.getElementById("game_stats").textContent = "Draw!";
@@ -88,15 +69,9 @@ function checkState() {
 }
 function wrapperButton(number) {
 	if (state[number] == 0) {
-		if (currentPlayer == 0) {
-			currentPlayer = 1;
-			state[number] = 1;
-			document.getElementById("Buttons").children[Math.floor(number / 3)].children[number % 3].children[0].textContent = "X";
-		} else {
-			currentPlayer = 0;
-			state[number] = 2;
-			document.getElementById("Buttons").children[Math.floor(number / 3)].children[number % 3].children[0].textContent = "O";
-		}
+		state[number] = currentPlayer + 1;
+		currentPlayer = (currentPlayer + 1) % 2;
+		document.getElementById("Buttons").children[Math.floor(number / 3)].children[number % 3].children[0].textContent = players[currentPlayer];
 		++moves;
 		checkState();
 	}
